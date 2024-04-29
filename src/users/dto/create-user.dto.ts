fb2f23ccd43988ae419bc65f1b97e209d694f094
Éverton Carvalho <evertonsnake@gmail.com>
2024-04-28 23:50:04 -0300

    swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'Email do usuário' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Nome do usuário' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Definir se usuario é administrador do usuário',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
